7288fa7cf00bd7016483fc852c4c1c01ec0f2233
Santiago Cecconato <santycheco1@gmail.com>
2022-04-08 20:45:47 -0300

    ej 9

--- TP2_9/Eje9/Eje9_2.py
+++ TP2_9/Eje9/Eje9_2.py
@@ -1,5 +1,3 @@
-
-
 # verifica si la posicin es valida para contar a ambos lados
 # y cuento minas cercanas
 def verificar_char_2(num_lin, num_char, celdas):
@@ -32,17 +30,17 @@ def verificar_char(num_lin, num_char, celdas):
 
 # verifica si las lineas son validas para contar
 # y realizo cambio en celdas_2
-def verificar_linea(num_lin, num_char, celdas_2, celdas):
+def verificar_linea(linea, letra, celdas_2, celdas):
     numero = 0
-    if num_lin >= 1:  # si no entra estoy en la linea 1
-        numero += verificar_char(num_lin - 1, num_char, celdas)
+    if celdas.index(linea) >= 1:  # si no entra estoy en la linea 1
+        numero += verificar_char(celdas[celdas.index(linea) -1], letra, celdas)
 
-    numero += verificar_char_2(num_lin, num_char, celdas)  # solo veridica el char a los costados y suma
+    numero += verificar_char_2(linea, letra, celdas)  # solo veridica el char a los costados y suma
 
-    if num_lin < len(celdas):  # si no es la ultima linea
-        numero += verificar_char(num_lin + 1, num_char, celdas)
+    if celdas.index(linea) < len(celdas):  # si no es la ultima linea
+        numero += verificar_char(celdas[celdas.index(linea) +1], letra, celdas)
 
-    celdas_2[num_lin][num_char] = chr(numero)
+    celdas_2[celdas.index(linea)][celdas.index(linea).] = chr(numero)
 
 
 # evaluo si el lugar tiene una mina o no,
@@ -58,20 +56,16 @@ celdas_2 = celdas.copy()
 # celdas_2 = list(filter(lambda x: list(x), celdas_2))
 for indice in range(len(celdas_2)):
     celdas_2[indice] = list(celdas_2[indice])
-print(len(celdas_2[2]))
+
 
 # 1º filter itera cada strs de la list celdas
 # 2º filter itera cada char del str(enviado por el prmer filter)
 # celda_final = filter(lambda x: filter(lambda c: devolver_numero(c, celdas, celdas_2), x), celdas)
-num_lin = 0
-for linea in celdas:
 
-    num_letra = 0
+for linea in celdas:
+    print(celdas.index(linea))
     for letra in linea:
         if not is_mina(letra):
-            verificar_linea(num_lin, num_letra, celdas_2, celdas)  # envia la posicion del - en la lista como parametro
-        num_letra += 1
-
-    num_letra += 1
+            verificar_linea(linea, letra, celdas_2, celdas)  # envia la posicion del - en la lista como parametro
 
 print(celdas_2)
